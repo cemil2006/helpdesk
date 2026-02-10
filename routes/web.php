52140ef146d590cb6ac366272f9233eed415e039
cemil <cemil3456@icloud.com>
2026-02-10 12:58:41 +0100

    views maken en de files.blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+    Route::get('categories/index', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
