d01a9b628c2a79af55710d0e7c9bb28bf97ca68c
JustinBiez <199956@novacollege.nl>
2026-02-12 13:38:16 +0100

    Implement ticket management features: add index view, create TicketSeeder, and update routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TicketController; 
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,3 +12,10 @@ Route::get('/', function () {
     Route::get('categories/edit', [CategoryController::class, 'edit'])->name('categories.edit');
     Route::get('categories/update', [CategoryController::class, 'update'])->name('categories.update');
     Route::get('categories/delet', [CategoryController::class, 'delete'])->name('categories.delete');
+
+    Route::get('tickets/index', [TicketController::class, 'index'])->name('tickets.index');
+    Route::get('tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+    Route::get('tickets/store', [TicketController::class, 'store'])->name('tickets.store'); 
+    Route::get('tickets/edit', [TicketController::class, 'edit'])->name('tickets.edit');
+    Route::get('tickets/update', [TicketController::class, 'update'])->name('tickets.update');
+    Route::get('tickets/delet', [TicketController::class, 'delete'])->name('tickets.delete');
