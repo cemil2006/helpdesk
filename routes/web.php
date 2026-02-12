33b308b2265a35ac322b1904c5c3da2c90857c24
cemil <cemil3456@icloud.com>
2026-02-12 12:39:15 +0100

    webs van crud categorie

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,6 @@ Route::get('/', function () {
     Route::get('categories/index', [CategoryController::class, 'index'])->name('categories.index');
     Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
     Route::get('categories/store', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('categories/edit', [CategoryController::class, 'edit'])->name('categories.edit');
     Route::get('categories/update', [CategoryController::class, 'update'])->name('categories.update');
+    Route::get('categories/delet', [CategoryController::class, 'delete'])->name('categories.delete');
