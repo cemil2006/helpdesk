791b0be996fc7d4fa2b42fd90d4d35b2ee696780
cemil <cemil3456@icloud.com>
2026-03-11 14:02:04 +0100

    de routes voor ticketstatuses gemaakt

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,33 @@
 
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TicketController; 
+use App\Http\Controllers\TicketController;
 use App\Http\Controllers\TicketStatusController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-    Route::get('categories/index', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('categories/show/{category}', [CategoryController::class, 'show'])->name('categories.show');
-    Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('categories/store', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('categories/edit/{category}', [CategoryController::class, 'edit'])->name('categories.edit');
-    Route::put('categories/update/{category}', [CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('categories/destroy/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+Route::get('categories/index', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('categories/show/{category}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('categories/store', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('categories/edit/{category}', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('categories/update/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('categories/destroy/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-    Route::get('tickets/index', [TicketController::class, 'index'])->name('tickets.index');
-    Route::get('tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-    Route::post('tickets/store', [TicketController::class, 'store'])->name('tickets.store'); 
-    Route::get('tickets/edit/{ticket}', [TicketController::class, 'edit'])->name('tickets.edit');
-    Route::put('tickets/update/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
-    Route::delete('tickets/destroy/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+Route::get('tickets/index', [TicketController::class, 'index'])->name('tickets.index');
+Route::get('tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+Route::post('tickets/store', [TicketController::class, 'store'])->name('tickets.store');
+Route::get('tickets/edit/{ticket}', [TicketController::class, 'edit'])->name('tickets.edit');
+Route::put('tickets/update/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
+Route::delete('tickets/destroy/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
 
-    Route::get('ticket-statuses/index', [TicketStatusController::class, 'index'])->name('ticket-statuses.index');
-    Route::get('ticket-statuses/create', [TicketStatusController::class, 'create'])->name('ticket-statuses.create');
-    Route::get('ticket-statuses/edit/{ticketStatus}', [TicketStatusController::class, 'edit'])->name('ticket-statuses.edit');
-    Route::post('ticket-statuses/store', [TicketStatusController::class, 'store'])->name('ticket-statuses.store');
-    
+Route::get('ticket-statuses/index', [TicketStatusController::class, 'index'])->name('ticket-statuses.index');
+Route::get('ticket-statuses/show/{ticket}', [TicketStatusController::class, 'show'])->name('ticket-statuses.show');
+Route::get('ticket-statuses/create', [TicketStatusController::class, 'create'])->name('ticket-statuses.create');
+Route::post('ticket-statuses/store', [TicketStatusController::class, 'store'])->name('ticket-statuses.store');
+Route::get('ticket-statuses/edit/{ticketStatus}', [TicketStatusController::class, 'edit'])->name('ticket-statuses.edit');
+Route::put('ticket-statuses/update/{ticket}', [TicketStatusController::class, 'update'])->name('ticket-statuses.update');
+Route::delete('ticket-statuses/destroy/{ticket}', [TicketStatusController::class, 'destroy'])->name('ticket-statuses.destroy');
+
